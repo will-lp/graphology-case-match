b0ec1eff902e28adf64596a1ec81cb968a751177
will-lp <will_lp@ymail.com>
2013-06-09 10:23:44 -0300

    Housekeeping.

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ file="graphology-case-match-extension.jar"
 
 echo " * Cleaning older version..."
 rm "dist/$file"
-rm "classes/ -R"
+rm classes/ -R
 rm "~/.groovy/lib/$file"
 
 echo " * Compiling..."
 groovyc src/org/graphology/extension/*.groovy -d "./classes/"
+cp src/META-INF -R classes/
 
 echo " * Jar-ing..."
 cd classes
@@ -22,10 +23,12 @@ cd ..
 #echo " * Copying to '.groovy/lib'"
 #cp dist/$file > ~/.groovy/lib/
 
-echo " * Launching tests"
+echo " * Launching tests..."
 groovy -cp dist/$file test/TestCase.groovy
 groovy -cp dist/$file test/TestCaseLazy.groovy
 groovy -cp dist/$file test/TestCaseCollect.groovy
 groovy -cp dist/$file test/TestCaseLazyCollect.groovy
 groovy -cp dist/$file test/TestStatic.groovy
 
+echo " * Copying to .groovy/lib..."
+cp "dist/$file" ~/.groovy/lib
